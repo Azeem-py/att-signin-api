02faa8b9f8039a41d9369153738a6b206f3c837a
Azeem-py <74514285+Azeem-py@users.noreply.github.com>
2024-03-21 09:33:42 +0100

    added new url to cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const corsOptions = {
     'https://att-signin.vercel.app',
     'https://one-note-welcome.vercel.app',
     'https://welcome-at.vercel.app',
-    'https://att-auth.onrender.com',
+    'https://service-koo.vercel.app',
     'https://welcomeat.vercel.app',
   ],
   credentials: true,
